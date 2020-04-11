1aed0b407b666babb183c4296a6b6c8c1ec9afff
Teodor Angelovski <angelovski.teo@outlook.com>
2020-04-11 15:30:03 +0200

    points class finished

--- Point.java
+++ Point.java
@@ -3,20 +3,54 @@ class Point {
 	double x,y;
 	String color;
 
-	//TODO add new variable
+	public Point(String id, double x, double y, String color){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
 
-	//TODO constructor
+	public String getId() {return this.id;}
 
-	//TODO setters and getters
+	public void setId(String id) {this.id = id;}
+
+	public double getX(){return this.x;}
+
+	public void setX(double x){this.x = x;}
+
+	public double getY(){return this.y;}
+
+	public void setY(double y){this.y = y;}
+
+	public String getColor() {return this.color;}
 
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		//	L,R,U,D
+		
+		//	X
+		if (xDirection == 'L') {
+			this.x--;
+		}
+		else if (xDirection == 'R') {
+			this.x++;
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
+
+		//	Y
+		if (yDirection == 'U') {
+			this.y++;
+		}
+		else if (yDirection == 'D') {
+			this.y--;
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public void draw () {
-		//TODO
+		system.out.println(String.format("%s: (%f, %f) is %s", this.id, this.x, this.y, this.color));
 	}
-
-
-
 }
